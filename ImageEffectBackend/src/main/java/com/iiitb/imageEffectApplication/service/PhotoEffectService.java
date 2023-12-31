850576a4870013c62faeda0318e2f787862d8f16
varnit-mittal <Varnit.Mittal@iiitb.ac.in>
2024-01-01 04:14:52 +0530

    updated

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/PhotoEffectService.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/PhotoEffectService.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
-
+import com.iiitb.imageEffectApplication.effectImplementation.*;
 @Service
 public class PhotoEffectService {
     
