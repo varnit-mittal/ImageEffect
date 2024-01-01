6711595e0be15be956c5d7a24544c92abed92822
Tanish Pathania <120204495+Tanish-pat@users.noreply.github.com>
2024-01-01 18:39:50 +0530

    Updated commented LoggingService.java

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/LoggingService.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/LoggingService.java
@@ -1,155 +1,155 @@
-package com.iiitb.imageEffectApplication.service;
+// package com.iiitb.imageEffectApplication.service;
 
-import com.iiitb.imageEffectApplication.model.LogModel;
-import org.springframework.stereotype.Service;
+// import com.iiitb.imageEffectApplication.model.LogModel;
+// import org.springframework.stereotype.Service;
 
-import java.io.*;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
+// import java.io.*;
+// import java.time.LocalDateTime;
+// import java.util.ArrayList;
+// import java.util.Collections;
+// import java.util.List;
+// import java.util.Locale;
 
-@Service
-public class LoggingService {
+// @Service
+// public class LoggingService {
 
-    public void addLog(String fileName, String effectName, String optionValues) {
-        try
-        {
-            File file = new File("log.log");
-            if(!file.exists())
-            {
-                file.createNewFile();
-            }
+//     public void addLog(String fileName, String effectName, String optionValues) {
+//         try
+//         {
+//             File file = new File("log.log");
+//             if(!file.exists())
+//             {
+//                 file.createNewFile();
+//             }
 
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        LocalDateTime localDateTime = LocalDateTime.now();
-        String timestamp = localDateTime.toString();
-        try
-        {
-            FileWriter writer=new FileWriter("log.log",true);
-            BufferedWriter buffer = new BufferedWriter(writer);
-            buffer.write(effectName+","+optionValues+","+timestamp+","+fileName+"\n");
-            buffer.close();
-            writer.close();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
+//         }
+//         catch (IOException e)
+//         {
+//             e.printStackTrace();
+//         }
+//         LocalDateTime localDateTime = LocalDateTime.now();
+//         String timestamp = localDateTime.toString();
+//         try
+//         {
+//             FileWriter writer=new FileWriter("log.log",true);
+//             BufferedWriter buffer = new BufferedWriter(writer);
+//             buffer.write(effectName+","+optionValues+","+timestamp+","+fileName+"\n");
+//             buffer.close();
+//             writer.close();
+//         }
+//         catch (IOException e)
+//         {
+//             e.printStackTrace();
+//         }
+//     }
 
-    public List<LogModel> getAllLogs() {
+//     public List<LogModel> getAllLogs() {
 
-        List<LogModel> logs=new ArrayList<>();
-        try
-        {
-            File file = new File("log.log");
-            if(!file.exists())
-            {
-                file.createNewFile();
-            }
-            FileReader reader=new FileReader(file);
-            BufferedReader buffer = new BufferedReader(reader);
-            String line;
-            while((line=buffer.readLine())!=null)
-            {
-                String[] log=line.split(",");
-                logs.add(new LogModel(log[2],log[3],log[0],log[1]));
-            }
-            Collections.reverse(logs);
-            buffer.close();
-            reader.close();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        return logs;
-    }
+//         List<LogModel> logs=new ArrayList<>();
+//         try
+//         {
+//             File file = new File("log.log");
+//             if(!file.exists())
+//             {
+//                 file.createNewFile();
+//             }
+//             FileReader reader=new FileReader(file);
+//             BufferedReader buffer = new BufferedReader(reader);
+//             String line;
+//             while((line=buffer.readLine())!=null)
+//             {
+//                 String[] log=line.split(",");
+//                 logs.add(new LogModel(log[2],log[3],log[0],log[1]));
+//             }
+//             Collections.reverse(logs);
+//             buffer.close();
+//             reader.close();
+//         }
+//         catch (IOException e)
+//         {
+//             e.printStackTrace();
+//         }
+//         return logs;
+//     }
 
-    public List<LogModel> getLogsByEffect(String effectName) {
-        List<LogModel> logs=new ArrayList<>();
-        try
-        {
-            File file = new File("log.log");
-            if(!file.exists())
-            {
-                file.createNewFile();
-            }
-            FileReader reader=new FileReader(file);
-            BufferedReader buffer = new BufferedReader(reader);
-            String line;
-            while((line=buffer.readLine())!=null)
-            {
-                String[] log=line.split(",");
-                if(log[0].equalsIgnoreCase(effectName))
-                {
-                    logs.add(new LogModel(log[2],log[3],log[0],log[1]));
-                }
-            }
-            buffer.close();
-            reader.close();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        return logs;
-    }
+//     public List<LogModel> getLogsByEffect(String effectName) {
+//         List<LogModel> logs=new ArrayList<>();
+//         try
+//         {
+//             File file = new File("log.log");
+//             if(!file.exists())
+//             {
+//                 file.createNewFile();
+//             }
+//             FileReader reader=new FileReader(file);
+//             BufferedReader buffer = new BufferedReader(reader);
+//             String line;
+//             while((line=buffer.readLine())!=null)
+//             {
+//                 String[] log=line.split(",");
+//                 if(log[0].equalsIgnoreCase(effectName))
+//                 {
+//                     logs.add(new LogModel(log[2],log[3],log[0],log[1]));
+//                 }
+//             }
+//             buffer.close();
+//             reader.close();
+//         }
+//         catch (IOException e)
+//         {
+//             e.printStackTrace();
+//         }
+//         return logs;
+//     }
 
-    public void clearLogs() {
-        try
-        {
-            File file = new File("log.log");
-            if(!file.exists())
-            {
-                file.createNewFile();
-            }
-            FileWriter fw= new FileWriter(file, false);
-            fw.write("");
-            fw.close();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        return;
-    }
-    public List<LogModel> getLogsBetweenTimestamps(LocalDateTime startTime, LocalDateTime endTime)
-    {
-        List<LogModel> logsBetweenTimestamps= new ArrayList<>();
-        try{
-            File file = new File("log.log");
-            if(!file.exists())
-            {
-                file.createNewFile();
-            }
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line;
-            while ((line = bufferedReader.readLine()) != null){
-                String [] sp = line.split(",");
-                String timestamp = sp[2];
-                String fileName = sp[3];
-                String effectName = sp[0];
-                String optionValues = sp[1];
-                LocalDateTime effectTimeStamp = LocalDateTime.parse(timestamp);
-                if (effectTimeStamp.isAfter(startTime) && effectTimeStamp.isBefore(endTime))
-                {
-                    LogModel logModel = new LogModel(timestamp, fileName, effectName, optionValues);
-                    logsBetweenTimestamps.add(logModel);
-                }
-            }
-            bufferedReader.close();
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-        return logsBetweenTimestamps;
-    }
-}
+//     public void clearLogs() {
+//         try
+//         {
+//             File file = new File("log.log");
+//             if(!file.exists())
+//             {
+//                 file.createNewFile();
+//             }
+//             FileWriter fw= new FileWriter(file, false);
+//             fw.write("");
+//             fw.close();
+//         }
+//         catch (Exception e)
+//         {
+//             e.printStackTrace();
+//         }
+//         return;
+//     }
+//     public List<LogModel> getLogsBetweenTimestamps(LocalDateTime startTime, LocalDateTime endTime)
+//     {
+//         List<LogModel> logsBetweenTimestamps= new ArrayList<>();
+//         try{
+//             File file = new File("log.log");
+//             if(!file.exists())
+//             {
+//                 file.createNewFile();
+//             }
+//             FileReader fileReader = new FileReader(file);
+//             BufferedReader bufferedReader = new BufferedReader(fileReader);
+//             String line;
+//             while ((line = bufferedReader.readLine()) != null){
+//                 String [] sp = line.split(",");
+//                 String timestamp = sp[2];
+//                 String fileName = sp[3];
+//                 String effectName = sp[0];
+//                 String optionValues = sp[1];
+//                 LocalDateTime effectTimeStamp = LocalDateTime.parse(timestamp);
+//                 if (effectTimeStamp.isAfter(startTime) && effectTimeStamp.isBefore(endTime))
+//                 {
+//                     LogModel logModel = new LogModel(timestamp, fileName, effectName, optionValues);
+//                     logsBetweenTimestamps.add(logModel);
+//                 }
+//             }
+//             bufferedReader.close();
+//         }
+//         catch (IOException e){
+//             e.printStackTrace();
+//         }
+//         return logsBetweenTimestamps;
+//     }
+// }
