d6e7acf355d513edda95a8807de06657f006f9de
Tanish Pathania <120204495+Tanish-pat@users.noreply.github.com>
2024-01-01 19:13:02 +0530

    Update GrayScale.java

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementation/GrayScale.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementation/GrayScale.java
@@ -1,16 +1,3 @@
-// package com.iiitb.imageEffectApplication.effectImplementation;
-
-// import com.iiitb.imageEffectApplication.baseEffects.PhotoEffect;
-// import com.iiitb.imageEffectApplication.service.LoggingService;
-// import libraryInterfaces.Pixel;
-// import libraryInterfaces.GrayscaleInterface;
-
-// public class GrayScale implements PhotoEffect{
-//     public Pixel[][] apply(Pixel[][] image, String fileName, LoggingService loggingService){
-//         loggingService.addLog(fileName, "Grayscale", "N/A");
-//         return GrayscaleInterface.applyGrayscale(image);
-//     }
-// }
 // Package declaration for the Grayscale effect implementation.
 package com.iiitb.imageEffectApplication.effectImplementation;
 
