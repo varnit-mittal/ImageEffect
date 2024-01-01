df8edb363f8c18cb5cd34348477cd59bbf829e6b
Tanish Pathania <120204495+Tanish-pat@users.noreply.github.com>
2024-01-01 19:13:52 +0530

    Update DominantColor.java

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementation/DominantColor.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementation/DominantColor.java
@@ -1,16 +1,3 @@
-// package com.iiitb.imageEffectApplication.effectImplementation;
-
-// import com.iiitb.imageEffectApplication.service.LoggingService;
-// import com.iiitb.imageEffectApplication.baseEffects.PhotoEffect;
-// import libraryInterfaces.Pixel;
-// import libraryInterfaces.DominantColourInterface;
-
-// public class DominantColor implements PhotoEffect {
-//     public Pixel[][] apply(Pixel[][] image, String fileName, LoggingService loggingService){
-//         loggingService.addLog(fileName, "Dominant Colour", "N/A");
-//         return DominantColourInterface.applyDominantColour(image);
-//     }
-// }
 // Package declaration for the Dominant Color effect implementation.
 package com.iiitb.imageEffectApplication.effectImplementation;
 
