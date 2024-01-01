1246c14700765d43dfe51f74df62c86bd756dcc9
Tanish Pathania <120204495+Tanish-pat@users.noreply.github.com>
2024-01-01 19:12:06 +0530

    Update Invert.java

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementation/Invert.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementation/Invert.java
@@ -1,16 +1,3 @@
-// package com.iiitb.imageEffectApplication.effectImplementation;
-
-// import com.iiitb.imageEffectApplication.baseEffects.PhotoEffect;
-// import com.iiitb.imageEffectApplication.service.LoggingService;
-// import libraryInterfaces.Pixel;
-// import libraryInterfaces.InvertInterface;
-
-// public class Invert implements PhotoEffect{
-//     public Pixel[][] apply(Pixel[][] image, String fileName, LoggingService loggingService){
-//         loggingService.addLog(fileName, "Invert", "N/A");
-//         return InvertInterface.applyInvert(image);
-//     }
-// }
 // Package declaration for the Invert effect implementation.
 package com.iiitb.imageEffectApplication.effectImplementation;
 
