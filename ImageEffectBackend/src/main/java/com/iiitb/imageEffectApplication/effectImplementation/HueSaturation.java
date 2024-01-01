2af2db2d3f7e5fc8272bf6eb16a7fcf3295f2c88
Tanish Pathania <120204495+Tanish-pat@users.noreply.github.com>
2024-01-01 19:12:51 +0530

    Update HueSaturation.java

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementation/HueSaturation.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementation/HueSaturation.java
@@ -1,31 +1,3 @@
-// package com.iiitb.imageEffectApplication.effectImplementation;
-
-// import com.iiitb.imageEffectApplication.baseEffects.ParameterizableEffect;
-// import com.iiitb.imageEffectApplication.service.LoggingService;
-// import libraryInterfaces.Pixel;
-// import libraryInterfaces.HueSaturationInterface;
-// import com.iiitb.imageEffectApplication.exception.IllegalParameterException;
-
-// public class HueSaturation implements ParameterizableEffect{
-//     private float hue;
-//     private float sat;
-//     public float getHue(){
-//         return this.hue;
-//     }
-//     public float getSat(){
-//         return this.sat;
-//     }
-//     public void setParameter( String str ,float val) throws  IllegalParameterException{
-//         if(val<0 || val>100) throw new IllegalParameterException("Illegal parameters. Parameters must be in the range 0 to 100");
-//         if(str.equals("H"))this.hue=val;
-//         else this.sat=val;
-//     }
-
-//     public Pixel[][] apply(Pixel[][] image, String fileName, LoggingService loggingService){
-//         loggingService.addLog(fileName, "Hue Saturation", "Hue = " + hue +" Saturation = "+ sat);
-//         return HueSaturationInterface.applyHueSaturation(image, hue, sat);
-//     }
-// }
 // Package declaration for the Hue Saturation effect implementation.
 package com.iiitb.imageEffectApplication.effectImplementation;
 
